03b7a9e245eb289a8fe15558fb01d52e9b00837c
Sung-27 <shj_27@naver.com>
2025-10-24 15:55:51 +0900

    State

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class State : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum States { Off, Normal,charge,FullCharged }
+
+    private States states = States.Normal;
+    private bool charging = false;
+    private float battery = 50.0f;
+
+    private void Update()
     {
-        
+        switch(states)
+        {
+            case States.Off:
+                OffUpate();
+                break;
+            case States.Normal:
+                NormalUpate();
+                break;
+            case States.FullCharged:
+                FullChargedUpdate();
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OffUpate()
     {
-        
+        if(charging)
+        {
+            states = States.charge;
+        }
+    }
+
+    private void NormalUpate()
+    {
+        battery -= 1.5f * Time.deltaTime;
+        if(charging)
+        {
+            states = States.charge;
+        }
+        else
+        {
+            states = States.Off;
+        }
+    }
+
+    private void FullChargedUpdate()
+    {
+        if (!charging)
+        {
+            states = States.Normal;
+        }
+    }
+
+    public void ConnectCharger()
+    {
+        charging = true;
+
+    }
+
+    public void DisConnectCharger()
+    {
+        charging = false;
     }
 }
