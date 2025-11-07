2a9ebe6432015bcc99b69a7503efaa997790a24a
Sung-27 <shj_27@naver.com>
2025-11-07 12:15:20 +0900

    UI시작

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] images;
-    [SerializeField] RectTransform imagesW;
-    [SerializeField] bool isImages = false;
+    [SerializeField] private GameObject[] images;
+
+   
+    [SerializeField] private Button[] buttons;
+
+    [SerializeField] public bool isImages = false;
+    [SerializeField] public bool isbuttons = false;
     Character character;
     private void Awake()
     {
@@ -16,6 +20,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            isImages = true;
+        }
+
         if (!isImages)
         {
             for (int i = 0; i < 3; i++)
@@ -24,7 +33,6 @@ public class GameManager : MonoBehaviour
                 if (character.weaponsituation == i)
                 {
                     images[i].SetActive(images[i]);
-                    images[i].transform.position = imagesW.position;
                 }
             }
         }
@@ -33,8 +41,23 @@ public class GameManager : MonoBehaviour
             for (int i = 0; i < 3; i++)
             {
                 images[i].SetActive(true);
+                isbuttons = true;
+            }
+        }
 
+        if (isbuttons)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                buttons[i].onClick.AddListener(MyClickFunction);
+                images[i].SetActive(images[i]);
             }
         }
     }
+    public void MyClickFunction()
+    {
+        character.weaponsituation = 1;
+        isbuttons = false;
+    }
+
 }
